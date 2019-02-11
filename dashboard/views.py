8172bb01c620a4a189184736ff88a8f687ec0318
Riajul Kashem <riajul.programmer@gmail.com>
2019-02-11 19:58:35 +0600

    Added pdfView(View) class for rendering pdf

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,14 @@
+from xhtml2pdf import pisa
+from django.template.loader import get_template
+from django.http import HttpResponse
+from io import BytesIO
 from django.shortcuts import redirect, render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import PasswordChangeView
 from django.contrib.auth import update_session_auth_hash
 from django.contrib.auth import authenticate, login
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, View
 from django.contrib.auth.models import User
 from results.models import DeclareResult
 from django.http import JsonResponse
@@ -19,10 +23,11 @@ from students.models import Student
 
 
 def index(request):
-    print('Inside Home Page')
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
+        print("\nUser Name = ",username)
+        print("Password = ",password)
         user = authenticate(username=username, password=password)
         if user is not None:
             login(request, user)
@@ -31,6 +36,7 @@ def index(request):
             context = {'message':'Invalid User Name and Password'}
             return render(request, 'index.html', context)
     return render(request, 'index.html', {'name': 'Riajul Kashem', 'pass': 'login@srms'})
+
 
 class DashboardView(LoginRequiredMixin,TemplateView):
     template_name = "dashboard.html"
@@ -48,15 +54,12 @@ class DashboardView(LoginRequiredMixin,TemplateView):
 def find_result_view(request):
     student_class = DeclareResult.objects.all()
     if request.method == "POST":
-        print("\nRequested From Post Data\n")
         data = request.POST
-        print(data)
         # data = json.loads(form)
         roll = int(data['rollid'])
         pk = int(data['class'])
         clss = get_object_or_404(DeclareResult, pk=pk)
         if clss.select_student.student_roll == roll:
-            print("\nMached POST Data\n")
             data = {
                 'pk': data['class']
             }
@@ -70,8 +73,15 @@ def find_result_view(request):
     return render(request, 'find_result.html', {'class':student_class})
 
 def result(request, pk):
-    r = get_object_or_404(DeclareResult, pk=pk)
-    return render(request, 'result.html', {'object':r, 'pk':pk})
+    object = get_object_or_404(DeclareResult, pk=pk)
+    lst = []
+    marks = []
+    for i in range(int(len(object.marks)/2)):
+        lst.append(object.marks['subject_'+str(i)])
+        lst.append(object.marks['subject_'+str(i)+'_mark'])
+        marks.append(lst)
+        lst = []
+    return render(request, 'result.html', {'object':object,'pk':pk, 'marks':marks})
 
 
 class PasswordChangeView(LoginRequiredMixin, PasswordChangeView):
@@ -84,5 +94,30 @@ class PasswordChangeView(LoginRequiredMixin, PasswordChangeView):
         context['main_page_title'] = 'Admin Change Password'
         context['panel_title'] = 'Admin Change Password'
         return context
-    
-    
+
+
+def renderPdf(template, content={}):
+    t = get_template(template)
+    send_data = t.render(content)
+    result = BytesIO()
+    pdf = pisa.pisaDocument(BytesIO(send_data.encode("ISO-8859-1")), result)
+    if not pdf.err:
+        return HttpResponse(result.getvalue(), content_type='application/pdf')
+    else:
+        return None
+
+class pdf(View):
+    def get(self, request, id):
+        try:
+            query = get_object_or_404(DeclareResult, id=id)
+        except:
+            Http404('Content Not Found')
+        marks = []
+        lst = []
+        for i in range(int(len(query.marks)/2)):
+            lst.append(query.marks['subject_'+str(i)])
+            lst.append(query.marks['subject_'+str(i)+'_mark'])
+            marks.append(lst)
+            lst = []
+        article_pdf = renderPdf('result.html', {'object': query, 'marks':marks})
+        return HttpResponse(article_pdf, content_type='application/pdf')
