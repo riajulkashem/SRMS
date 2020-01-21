757c3cb53a81583dcae9f6b9c643a910ba19a2ed
Riajul Kashem <riajul.programmer@gmail.com>
2020-01-21 15:33:48 +0600

    moved heroku to linux shared hosting cpanale

--- srms/urls.py
+++ srms/urls.py
@@ -1,24 +1,12 @@
-"""srms URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
+from dashboard import views
+from django.conf.urls.static import static
+from django.conf import settings
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
-from dashboard import views
+from django.contrib import admin
+
+admin.autodiscover()
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='home'),
@@ -31,4 +19,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
--- srms/wsgi.py
+++ srms/wsgi.py
@@ -4,15 +4,12 @@ WSGI config for srms project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
-
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'srms.settings')
 
